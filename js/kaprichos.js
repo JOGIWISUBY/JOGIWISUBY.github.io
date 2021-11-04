378c2663b14948752a23855320d7616f73380fd6
Byron Amaya <byronamaya96@gmail.com>
2021-11-04 16:55:07 -0600

    Update 3.5

--- js/kaprichos.js
+++ js/kaprichos.js
@@ -19,18 +19,20 @@ function mostrar()
     {
         case "api":
             // Utilizando la Api de Local Storage, podemos guardar y luegorecuperar un valor
-            localStorage['bienvenida'] = 'Transmite tus sentimientos con distinción y elegancia, exprésate con un detalle que contiene: 1 Canasta belen o 1 mi querido viejo , 1 globo por el costo de $60';
+            localStorage['bienvenida'] = 'Sorprende a papá en su día especial con nuestro combo compuesto por: '+
+            ' 1 caja de madera con vino y 1 caja de chocolates. Por un costo de $65';
             valor = localStorage['bienvenida'];
             break;
         case "array":
             // Utilizar notación de matrices o arreglos
-            localStorage['bienvenida'] = 'Para esa persona tan especial un detalle tierno y personal para impresionar, compuesto por: 1 caja con frutas, rosas y vino o 1 10 de mayo, mas globo con texto “feliz dia mamá”. Por un costo de $60';
+            localStorage['bienvenida'] = 'Sorprende a mamá y demuestra el amor y agradecimiento con nuestro combo compuesto por: '+
+            'caja #1 y una caja de chocolates por $80';
             valor = localStorage['bienvenida'];
             break;
         case "object":
             // Y claro, también es válido utilizar notación de propiedades de objetos */
-            localStorage.bienvenida = 'Un dulce sentimiento expresado en un romántico regalo...'+
-            'Compuesto por: 1 cono con rosas, 1 globo personalizado, 1 caja de chocolates. Por el costo de $50';
+            localStorage.bienvenida = 'Demuestra ese sentimiento de amor y sorprende a esa persona especial con nuestro combo compuesto por: '+
+            '1 rosa preservada (color a elección), 1 globo de corazón y 1 caja de chocolates. Por el costo de $65';
             valor = localStorage.bienvenida;
             break;
         default:
